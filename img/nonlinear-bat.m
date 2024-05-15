42fbf2de65c1824f00ed50cd97255ed40c74e0ac
Alexander Brown <alex.brown7711@gmail.com>
2024-05-15 15:20:49 -0600

    Some more cleanup and new figure

--- img/nonlinear-bat.m
+++ img/nonlinear-bat.m
@@ -29,55 +29,91 @@ function y_vec = calc_y (x_vec, a, D)
   endfor
 endfunction
 
+## usage: soc = slow (x)
+##
+##
+function soc = slow (x)
+  soc = 30.0 * x/3600;
+
+  for i = 1:length(x)
+    if (soc(i) >= 388.0)
+      soc(i) = 388.0;
+    endif
+  endfor
+endfunction
+
+## usage: soc = fast (x)
+##
+##
+function soc = fast (x)
+  soc =  910.95 * x/3600;
+
+  for i = 1:length(x)
+    if (soc(i) >= 388.0)
+      soc(i) = 388.0;
+    endif
+  endfor
+endfunction
+
 ########################################################################################################################
 # SCRIPT
 ########################################################################################################################
-D = 1
-x_vec = 0:D:650;
-x_vec_plot = 0:D*0.01:650*0.01;
+HR2SEC = 3600;
+SEC2HR = 1/HR2SEC;
+HR = 15;
+D = 1;
+x_vec = 0:D:HR*HR2SEC;
+x_vec_plot = SEC2HR * x_vec;
+
 
 ## Configure Plot
 set(0, "defaultlinelinewidth", 4);
 set(0, "defaultaxesfontsize", 16); % axes labels
 
 ## 0.01
-a = -0.01;
+a = -0.000025;
 y_vec = calc_y(x_vec, a, D);
-
-size(x_vec)
-size(y_vec)
-
-hold;
 
 p = plot(x_vec_plot,y_vec);
 
-## 0.03
-a = -0.03;
-y_vec = calc_y(x_vec, a, D);
+hold;
 
-size(x_vec_plot)
-size(y_vec)
+## 0.03
+a = -0.000043;
+y_vec = calc_y(x_vec, a, D);
 
 p = plot(x_vec_plot,y_vec);
 
 ## 0.05
-a = -0.05;
+a = -0.0005;
 y_vec = calc_y(x_vec, a, D);
 
 p = plot(x_vec_plot,y_vec);
 
 ## 0.1
-a = -0.1;
+a = -0.0012;
 y_vec = calc_y(x_vec, a, D);
 
 p = plot(x_vec_plot,y_vec);
 
+## Linear
+## y_vec = slow(x_vec);
+## p = plot(x_vec_plot,y_vec);
+##
+## y_vec = fast(x_vec);
+## p = plot(x_vec_plot,y_vec);
+##
+## e = 0.8*388;
+## line([0 HR], [e, e], "linestyle", "--", "color", "red");
+
 ## Legend
-l = legend({"-0.01", "-0.03", "-0.05", "-0.1"});
-set(l, "fontsize", 20, "location", "southeast");
+l = legend({"-000025", "-0.000045", "-0.0005", "-0.002"});
+## l = legend({"-000025",  "-0.0005", "-0.0012", "30", "910"});
+set(l, "fontsize", 15, "location", "southeast");
 
 xlabel ("Time [hr]");
 ylabel ("SOC [kWh]");
+axis([0 HR 0 390])
 
 ## Output plot
 print(1, "nonlinear-bat.png");
